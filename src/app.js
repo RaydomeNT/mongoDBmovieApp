d513c972b9307f492db08f74028b8f825cd7d33e
RaydomeNT <raydome85@gmail.com>
2022-07-20 13:15:20 +0100

    find

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+const { MONGO_CLIENT_EVENTS } = require("mongodb");
 const yargs = require("yargs");
 const { client, connection } = require("./db/connection");
 const Movie = require("./utils");
@@ -9,17 +10,32 @@ const app = async (yargsObj) => {
     const newMovie = new Movie(yargsObj.title, yargsObj.actor, yargsObj.year);
     await newMovie.add(collection);
     //add a movie from the terminal into an object and save in database
+
   } else if (yargsObj.read) {
     console.log("Read");
-    const results = await collection.find().toArray();
-    console.log(results);
+    const query = { title: yargsObj.title, actor: yargsObj.actor, year: yargsObj.year }
+    const options = {
+      sort: { year: yargsObj.year -1},
+      projection: { title: 1, actor: 1, year: 1 }
+    };
+    const collection = await collection.find(query, options);
+    console.log(query);
     //list items from database
+  
   } else if (yargsObj.update) {
     console.log("Updated");
     //update one database entry
+
   } else if (yargsObj.delete) {
-    console.log("Deleted");
+    const query = { title: yargsObj.title }
+    const result = await collection.deleteOne(query);
+    if (result.deletedCount === 1) {
+      console.log("Deleted one doc");
+    } else {
+      console.log("Nothing deleted");
+    }
     //delete one database entry
+  
   } else {
     console.log("Incorrect command");
   }
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,12 +1,18 @@
 class Movie {
-  constructor(title, actor = "Not specified") {
+  constructor(title, actor = "Not specified", year = "Unknown") {
     this.title = title;
     this.actor = actor;
+    this.year = year;
   }
   async add(collection) {
     await collection.insertOne(this);
     //need to save 'this' into database
   }
-}
+  async list(collection) {
+    return await collection.find( this ).toArray();
+  } 
+
+  }
+
 
 module.exports = Movie;
